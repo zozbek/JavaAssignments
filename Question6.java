765c00e0ffa03ae9dfe5a9dc3f69530a29bea5de
zozbek <71548680+zozbek@users.noreply.github.com>
2020-09-19 12:11:48 +0100

    Add files via upload

---
+++ Question6.java
@@ -0,0 +1,21 @@
+package day_2;
+
+public class Question6 {
+
+	public static void main(String[] args) {
+		System.out.println("   ^   ");
+		System.out.println("  / \\  ");
+		System.out.println(" /   \\ ");
+		System.out.println("/     \\");
+		System.out.println("|     |");
+		System.out.println("|     |");
+		System.out.println("|     |");
+		System.out.println("|     |");
+		System.out.println("|     |");
+		System.out.println("|     |");
+		System.out.println("|     |");
+		System.out.println("_______");
+
+	}
+
+}
